4aac3c3c510339b641cd3641cd6a57b3f89c811b
Allison Rodriguez <allisonrod0106@gmail.com>
2026-03-09 07:27:35 +0100

    Create View Files and Move Game Logic

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-view">
+    <StartScreen @start="goToPlay" />
+  </div>
+</template>
+
+<script>
+import StartScreen from "../components/StartScreen.vue"
+
+export default {
+  name: "HomeView",
+
+  components: {
+    StartScreen
+  },
+
+  methods: {
+    goToPlay() {
+      this.$router.push({ name: "play" })
+    }
+  }
+}
+</script>
